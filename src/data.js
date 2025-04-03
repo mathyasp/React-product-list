9d4cb9867dbd5c8ffe2e8e2136d38f6202c5eb15
mathyasp <mathyastp@gmail.com>
2025-04-03 15:27:52 -0400

    Count products per category with reduce

--- src/data.js
+++ src/data.js
@@ -6,4 +6,10 @@ const allCategories = data.map(item => item.category);
 // Filter unique categories
 const uniqueCategories = [...new Set(allCategories)];
 
+// Count products per category with reduce
+const categoryCounts = data.reduce((obj, item) => {
+  obj[item.category] = (obj[item.category] || 0) + 1;
+  return obj;
+}, {});
+
 export default data;
