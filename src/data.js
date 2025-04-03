6773073d43fa4d4324634e9bc2df704331b8e227
mathyasp <mathyastp@gmail.com>
2025-04-03 15:26:26 -0400

    Filter unique categories

--- src/data.js
+++ src/data.js
@@ -3,4 +3,7 @@ import data from './data.json';
 // Get all categories using map
 const allCategories = data.map(item => item.category);
 
+// Filter unique categories
+const uniqueCategories = [...new Set(allCategories)];
+
 export default data;
